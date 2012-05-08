839a22297aaf14cfd09140c694460a5cab1c49d0
Jeremy Clewell <jeremy@jeremyclewell.com>
2012-05-08 08:14:10 -0700

    Adding CSS

--- build/build.js
+++ build/build.js
@@ -2,12 +2,13 @@ var fs = require("fs");
 var md = require("node-markdown").Markdown;
 var pdf = require("node-wkhtml").pdf();
 
-fs.readFile("../resume.md", "ascii", function(err, data) {
+fs.readFile("../resume.md", "utf-8", function(err, data) {
 	if (err) {
 		console.log("Could not open file: %s", err);
 		process.exit(1);
 	}
-	fs.writeFile("../resume.html", md(data), function(err) {
+	var html = "<!DOCTYPE html><html><head><meta charset='UTF-8' /><title>Jeremy Austin Clewell</title><link rel='stylesheet' href='css/style.css'></head>" + md(data) + "</html>";
+	fs.writeFile("../resume.html", html, function(err) {
 		if (err) {		
 			console.log("Could not write file %s", err);	
 		} else {
